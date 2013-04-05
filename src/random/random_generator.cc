bb719d56b0840f44008e47088d5b2c48f8d7e49d
Paul R Staab <staab@bio.lmu.de>
2013-04-05 11:48:59 +0200

    fixed pair sampling algorithm

--- src/random/random_generator.cc
+++ src/random/random_generator.cc
@@ -1,16 +1,19 @@
 #include "random_generator.h"
 
-// Uniformly samples a number out of 0, ..., range - 1
+// Uniformly samples a number out of 0, ..., range-1
+// Distribution checked -Paul
 int RandomGenerator::sampleInt(int range) {
   return(static_cast<int>(this->sample()*range));
 }
 
-// Uniformly sets sample1/2 to two different elements of
-// 0, ..., size - 1
-void RandomGenerator::sampleTwoElements(int size, int *sample1, int *sample2) {
-  *sample1 = this->sampleInt(size - 2);
-  *sample2 = this->sampleInt(size - 1);
-  if (*sample1 == *sample2) *sample2 = size - 1;
+// Uniformly samples a pair of different elements of 0, ..., range-1
+// Distribution checked -Paul
+void RandomGenerator::sampleTwoElements(int range, int *sample1, int *sample2) {
+  *sample1 = this->sampleInt(range);
+  *sample2 = this->sampleInt(range-1);
+  if (*sample1 == *sample2) *sample2 = range - 1;
+  assert( 0 <= *sample1 && *sample1 < range );
+  assert( 0 <= *sample2 && *sample2 < range );
 }
 
 double RandomGenerator::sampleExpo(double lambda){
