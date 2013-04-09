4c9baf25da6ae2200d52dec230f69ced0087a1ca
Gerton Lunter <gerton.lunter@gmail.com>
2013-04-09 23:32:24 +0100

    included fast exp; included sampling from exp changing rate; need unit tests

--- src/random/random_generator.cc
+++ src/random/random_generator.cc
@@ -1,10 +1,81 @@
 #include "random_generator.h"
 
+
+void RandomGenerator::set_seed(const int &seed){
+  this->seed_ = seed;
+  this->unit_exponential_ = -std::log(this->sample());
+}
+
+
+// Samples from an exponential distribution 
+// Distribution checked -Paul
+double RandomGenerator::sampleExpo(double lambda){
+  return -std::log(this->sample()) / lambda;
+}
+
+
+// Samples from an exponential distribution; return -1 if beyond limit
+// If a limit is known, this version is faster than the standard one
+double RandomGenerator::sampleExpoLimit(double lambda, double limit){
+  if (unit_exponential >= limit * lambda) {
+    unit_exponential -= limit * lambda;
+    return -1;
+  } else {
+    double result = unit_exponential / lambda;
+    unit_exponential = -std::log(this->sample());
+    return result;
+  }
+}
+
+
+// Samples waiting time, with limit, for a process with an exponentially changing rate:
+//  rate(t) = b exp( c t )
+// For a p ~ unif(0,1), a waiting time sample is (1/c) log[ 1 - (c/b) log p ]
+// It returns -1 if no event occurred; this can happen even if limit == +infinity (if c<0)
+double RandomGenerator::sampleExpoExpoLimit(double b, double c, double limit){
+  assert (b>0);
+  assert (c!=0);
+  // for any c, the no-event condition (t=maximum waiting time) is
+  //  (b/c) (exp(c t)-1) < -log p
+  // For c<0 and c>0 respectively this becomes
+  //  -c log p < b (exp(c t)-1)
+  //  -c log p > b (exp(c t)-1)  resp.
+  // These are implied by the conditions
+  //  -c log p < b (exp_up(c t)-1)    [c<0]
+  //  -c log p > b (exp_lo(c t)-1)    [c>0]
+  // where exp_up and exp_lo are upper and lower bounds for the exp(x) function resp.
+  if (c<0) {
+    double c_logp_limit = b*(EXP_UP(c*limit)-1);  // negative
+    if (c*unit_exponential < c_logp_limit) {
+      unit_exponential -= c_logp_limit / c;
+      return -1;
+    }
+    double y = 1.0 + c*unit_exponential / b;
+    unit_exponential = -std::log(this->sample());
+    if (y <= 0.0) return -1; // no event at all
+    y = std::log(y)/c;       
+    if (y>limit) return -1;  // the event time; can still be beyond limit
+    return y;
+  } else {
+    double c_logp_limit = b*(EXP_UP(c*limit)-1);  // positive
+    if (c*unit_exponential > c_logp_limit) {
+      unit_exponential -= c_logp_limit / c;
+      return -1;
+    }
+    double y = std::log( 1.0 + c*unit_exponential / b ) / c;
+    unit_exponential = -std::log(this->sample());
+    if (y>limit) return -1;
+    return y;
+  }
+}
+
+    
 // Uniformly samples a number out of 0, ..., range-1
 // Distribution checked -Paul
 int RandomGenerator::sampleInt(int range) {
   return(static_cast<int>(this->sample()*range));
 }
+
 
 // Uniformly samples a pair of different elements of 0, ..., range-1
 // Distribution checked -Paul
@@ -17,12 +88,3 @@ void RandomGenerator::sampleTwoElements(int range, int *sample1, int *sample2) {
 }
 
 
-// Samples from an exponential distribution 
-// Distribution checked -Paul
-double RandomGenerator::sampleExpo(double lambda){
-  return -std::log(this->sample()) / lambda;
-}
-
-void RandomGenerator::set_seed(const int &seed){
-  this->seed_ = seed;
-}
--- src/random/random_generator.h
+++ src/random/random_generator.h
@@ -7,8 +7,8 @@
 class RandomGenerator
 {
   public:
-   RandomGenerator() {};
-   virtual ~RandomGenerator() {};
+   RandomGenerator() { unit_exponential_ = 0.5; }
+   virtual ~RandomGenerator() {}
  
    //Getters & Setters
    int seed() { return this->seed_; }
@@ -28,7 +28,42 @@ class RandomGenerator
 #endif
 
   protected:
+   // seed
    int seed_;
+   // cache for a unit-exponentially distributed variable
+   double unit_exponential_;
 };
+
+
+// Fast and pretty tight upper and lower bounds for exp(x)
+// A quick test showed a ~10x speed improvement
+// See: Nicol N. Schraudolf, A Fast, Compact Approximation of the Exponential Function, Neural Computation 11, 853-862 (1999)
+// http://nic.schraudolph.org/pubs/Schraudolph99.pdf
+
+class _DoubleBits {
+  union {
+    double d;
+    struct {
+#ifdef BIG_ENDIAN
+    int j, i;
+#else
+    int i, j;
+#endif
+    } n;
+  } _double_bits;
+public:
+  _DoubleBits( int i ) {
+    _double_bits.n.i = i;
+    _double_bits.n.j = 0;
+  }
+  double value() const { return _double_bits.d; }
+};
+
+#define EXP_A (1048576/M_LN2) 
+#define EXP_C_UP 90253  /* for upper bound to exp */
+#define EXP_C_LO -1     /* for lower bound to exp */
+#define EXP_UP(y) ( _DoubleBits(EXP_A*(y) + (1072693248 - EXP_C_UP)).value() ) // upper bound to exp; at most 6.148% too high
+#define EXP_LO(y) ( _DoubleBits(EXP_A*(y) + (1072693248 - EXP_C_LO)).value() ) // lower bound to exp; at most 5.792% too low
+
 
 #endif
