3f0d0e7a5bd1a098584a46fef8bb57b1da760896
joezhu <sha.joe.zhu@gmail.com>
2013-09-01 15:39:10 +0100

    restore ms_vs_scrm-seg.sh

--- tests/ms_vs_scrm-seg.sh
+++ tests/ms_vs_scrm-seg.sh
@@ -1,99 +1,84 @@
 #!/bin/bash
 
-mkdir test-SEG
-cd test-SEG
+###########################
+
+mkdir test-seg
+cd test-seg
 rm *pdf
 
+mst=(10 20 50 100)
+msNsample=(2 3 7 10)
+
+#mst=(10)
+#msNsample=(3)
 
 rep=100000
+#npop=20000
 
-## compare SEG data
+## compare number of segregating sites
 compareSEG=compareSEG
-rm ${compareSEG}
+echo -e "compare number of mutations for ${rep} replicates \n\t\t|\t\t\t|\tms \t\t|\t scrm\t\t|\tTest1\t\t\t|\tTest2\t\t\t
+Nsam\ttheta\t|\tmean\tstdv\t|\tmean\tstdv\t|\tmean\tstdv \t|\tstats\tp-value\t\t|\tstats\tp-value\t\t" >${compareSEG}
 
-theta=10
+for t in "${mst[@]}"
+	do
+	for nsam in "${msNsample[@]}"
+		do
+		prefix=${nsam}sample${t}mut
+		#out=${prefix}out
+		nseg=${prefix}NumOfSeg
+		ms ${nsam} ${rep} -t ${t} -T | tail -n +4 | grep -v "//" | grep "segsites" | sed -e "s/segsites: //" > ms${nseg}
 
+		scrm ${nsam} ${rep} -t ${t} | tail -n +4 | grep -v "//"  | grep "segsites" | sed -e "s/segsites: //" > scrm${nseg}
 
-echo "rm(list=ls());
-#source(\"../fun_src.r\");
-figuretitle=scan(\"figuretitle\",what=\"\");
-currentcase=scan(\"current_case\",what=\"\");
-msdata=read.table(paste(\"ms\",\"data\",sep=\"\"))\$V1;
-scrmdata=read.table(paste(\"scrm\",\"data\",sep=\"\"))\$V1;
-#ee=1#ee_tmrca(${nsam});
-#sdv=1#sd_tmrca(${nsam});
-test=ks.test(msdata,scrmdata)
-pdf(paste(currentcase,figuretitle,\".pdf\",sep=\"\"));
-plot(ecdf(msdata), xlim=range(c(msdata, scrmdata)),col=\"red\", main=currentcase)
-plot(ecdf(scrmdata), add=TRUE, lty=\"dashed\", col=\"blue\")
-legend(\"bottomright\",c(paste(\"Tests Statistics = \",test\$statistic,sep=\"\"), paste(\"p-value = \",format(test\$p.value,digits=4),sep=\"\")))
+		echo "rm(list=ls());
+		source(\"../fun_src.r\");
+		msdata=read.table(\"ms${nseg}\")\$V1;
+		scrmdata=read.table(\"scrm${nseg}\")\$V1;
+		ee=ee_seg(${nsam},${t});
+		sdv=sd_seg_norecomb(${nsam},${t});
+		datamax=max(msdata,scrmdata);
+		mstable=table(factor(msdata,levels=1:datamax))
+		scrmtable=table(factor(scrmdata,levels=1:datamax))
+pdf(\"${nsam}sample${t}mut_NUM_MUT.pdf\");
+plot(as.numeric(names(mstable)), mstable/length(msdata),pch=16,col=\"red\",ylab=\"Frequency\",xlab=\"Number of mutations\");
+points(as.numeric(names(scrmtable)), scrmtable/length(scrmdata),pch=16,col=\"blue\")
+
+ms_newtable=table(msdata);
+scrm_mstable=table(factor(scrmdata,levels=names(table(msdata))));
+combined_scrm_ms_test=chisq.test(cbind(scrm_mstable, ms_newtable));
+
+scrm_newtable=table(scrmdata);
+ms_scrmtable=table(factor(msdata,levels=names(table(scrmdata))));
+combined_ms_scrm_test=chisq.test(cbind(scrm_newtable, ms_scrmtable));
+
+legend(\"topright\",c(paste(\"Test 1 Statistics = \",combined_scrm_ms_test\$statistic,sep=\"\"), paste(\"p-value = \",format(combined_scrm_ms_test\$p.value,digits=4),sep=\"\"),paste(\"Test 2 Statistics = \",combined_ms_scrm_test\$statistic,sep=\"\"), paste(\"p-value = \",format(combined_ms_scrm_test\$p.value,digits=4),sep=\"\")))
 legend(\"topleft\",c(\"ms\",\"scrm\"), col=c(\"red\",\"blue\"), pch=16)
+
 dev.off();
-#cat(paste(currentcase,\"|\",format(ee,digits=4),format(sdv,digits=4),\"|\",
-cat(paste(currentcase, figuretitle , \"\n\",\"|\",
-format(mean(msdata),scientific = TRUE),format(sd(msdata),scientific = TRUE),\"|\",
-format(mean(scrmdata),scientific = TRUE),format(sd(scrmdata),scientific = TRUE),\"|\",format(test\$statistic,scientific = TRUE),format(test\$p.value,scientific = TRUE), 
-sep=\"\t\"),file=\"${compareSEG}\",append=TRUE);cat(\"\n\",file=\"${compareSEG}\",append=TRUE);" > dummy.r
 
-foo(){
-	cut -f 2 ms_stats > msdata
-	cut -f 2 scrm_stats > scrmdata
-	echo "Pairewise_difference" > figuretitle
-	R CMD BATCH dummy.r
-	
-	cut -f 6 ms_stats > msdata
-	cut -f 6 scrm_stats > scrmdata
-	echo "Tajima_D" > figuretitle
-	R CMD BATCH dummy.r
-	
-	cut -f 8 ms_stats > msdata
-	cut -f 8 scrm_stats > scrmdata
-	echo "theta_H" > figuretitle
-	R CMD BATCH dummy.r
-	
-	cut -f 10 ms_stats > msdata
-	cut -f 10 scrm_stats > scrmdata
-	echo "H" > figuretitle
-	R CMD BATCH dummy.r
-	}
-
-#case 1 
-echo "4_samples" > current_case
-
-ms 4 ${rep} -t ${theta} | sample_stats > ms_stats
-scrm 4 ${rep} -t ${theta} | sample_stats > scrm_stats
-
-foo
+cat(paste(${nsam},${t},\"|\",format(ee,digits=4),format(sdv,digits=4),\"|\",
+format(mean(msdata),digits=4),format(sd(msdata),digits=4),\"|\",
+format(mean(scrmdata),digits=4),format(sd(scrmdata),digits=4),\"|\",
+format(combined_scrm_ms_test\$statistic,digits=4),format(combined_scrm_ms_test\$p.value,scientific = TRUE),\"|\",
+format(combined_ms_scrm_test\$statistic,digits=4),format(combined_ms_scrm_test\$p.value,scientific = TRUE),
+sep=\"\t\"),file=\"${compareSEG}\",append=TRUE);cat(\"\n\",file=\"${compareSEG}\",append=TRUE);
+" > dummy.r
+		R CMD BATCH dummy.r
+		rm ms${nseg}  scrm${nseg}
+		done
+	done
 
 
-#case 2
 
-echo "5_samples" > current_case
 
-ms 5 ${rep} -t ${theta} | sample_stats > ms_stats
-scrm 5 ${rep} -t ${theta} | sample_stats > scrm_stats
 
-foo
+#chistat=0
+#for (i in (1:dim(combinedtable)[1])){
+	#for (j in (1:dim(combinedtable)[2])){
+		#E=sum(combinedtable[i,])*sum(combinedtable[,j])/sum(combinedtable)
+#sum(combinedtable[i,])*sum(combinedtable[,j])/sum(combinedtable)
+		#chistat = chistat + (E-combinedtable[i,j])^2/E
+	#}
 
-#case 3
-
-#2 sub population, 6 samples from subpopulation 1, and 5 samples from subpopulation 2, with rate 10 from 1 to 2, and rate 5 from 2 to 1
-
-prefix="2groups6sam5sam_mig_x_10_5_x"
-echo ${prefix} > current_case
-	
-ms 4 ${rep} -t ${theta} -I 2 2 2 -ma x 10.0 5.0 x | sample_stats > ms_stats
-scrm 4 ${rep} -t ${theta} -I 2 2 2 -ma x 10.0 5.0 x | sample_stats > scrm_stats
-
-foo
-
-#case 4
-#3 sub population, 10 samples from subpopulation 1, and 4 samples from subpopulation 2, and 1 sample from the third with rate matrix on manual page 5
-
-prefix="3groups10sam4sam1sam_mig_x123x456x"
-echo ${prefix} > current_case
-	
-ms 15 ${rep} -t ${theta} -I 3 10 4 1 -ma x 1.0 2.0 3.0 x 4.0 5.0 6.0 x | sample_stats > ms_stats
-scrm 15 ${rep} -t ${theta} -I 3 10 4 1 -ma x 1.0 2.0 3.0 x 4.0 5.0 6.0 x | sample_stats > scrm_stats
-
-foo
+#}
