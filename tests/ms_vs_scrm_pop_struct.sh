159f1b9a7b1802b6305786fea40ee4e63fad2d3b
joezhu <sha.joe.zhu@gmail.com>
2013-09-03 14:13:29 +0100

    update testing file for population structure

--- tests/ms_vs_scrm_pop_struct.sh
+++ tests/ms_vs_scrm_pop_struct.sh
@@ -5,7 +5,7 @@ cd test-POP
 rm *pdf
 
 
-rep=100000
+rep=10000
 
 ## compare population sturture for a single population data
 comparePop=comparePop
@@ -13,6 +13,60 @@ rm ${comparePop}
 
 theta=10
 
+	echo "rm(list=ls());
+currentcase=scan(\"current_case\",what=\"\");
+figuretitle=scan(\"figuretitle\",what=\"\");
+
+msdata=read.table(\"ms${tmrca}\")\$V1;
+scrmdata=read.table(\"scrm${tmrca}\")\$V1;
+test=ks.test(msdata,scrmdata)
+pdf(paste(currentcase,figuretitle,\".pdf\",sep=\"\"));
+plot(ecdf(msdata), xlim=range(c(msdata, scrmdata)),col=\"red\", main=paste($nsam,\"sample \",figuretitle, \" KS test\",sep=\"\"))
+plot(ecdf(scrmdata), add=TRUE, lty=\"dashed\", col=\"blue\")
+legend(\"bottomright\",c(paste(\"Tests Statistics = \",test\$statistic,sep=\"\"), paste(\"p-value = \",format(test\$p.value,digits=4),sep=\"\")))
+legend(\"topleft\",c(\"ms\",\"scrm\"), col=c(\"red\",\"blue\"), pch=16)
+dev.off();
+cat(paste(${nsam},\"|\",format(ee,digits=4),format(sdv,digits=4),\"|\",
+format(mean(msdata),digits=4),format(sd(msdata),digits=4),\"|\",
+format(mean(scrmdata),digits=4),format(sd(scrmdata),digits=4),\"|\",test\$statistic,format(test\$p.value,digits=4), 
+sep=\"\t\"),file=\"${comparePop}\",append=TRUE);cat(\"\n\",file=\"${comparePop}\",append=TRUE);" > tmrca.r
+
+		echo "rm(list=ls());
+		#source(\"../fun_src.r\");
+figuretitle=scan(\"figuretitle\",what=\"\");
+currentcase=scan(\"current_case\",what=\"\");
+msdata=read.table(paste(\"ms\",\"data\",sep=\"\"))\$V1;
+scrmdata=read.table(paste(\"scrm\",\"data\",sep=\"\"))\$V1;
+		#ee=ee_seg(${nsam},${t});
+		#sdv=sd_seg_norecomb(${nsam},${t});
+		datamax=max(msdata,scrmdata);
+		mstable=table(factor(msdata,levels=1:datamax))
+		scrmtable=table(factor(scrmdata,levels=1:datamax))
+pdf(paste(currentcase,figuretitle,\".pdf\",sep=\"\"));
+plot(as.numeric(names(mstable)), mstable/length(msdata),pch=16,col=\"red\",ylab=\"Frequency\",xlab=figuretitle);
+points(as.numeric(names(scrmtable)), scrmtable/length(scrmdata),pch=16,col=\"blue\")
+
+ms_newtable=table(msdata);
+scrm_mstable=table(factor(scrmdata,levels=names(table(msdata))));
+combined_scrm_ms_test=chisq.test(cbind(scrm_mstable, ms_newtable));
+
+scrm_newtable=table(scrmdata);
+ms_scrmtable=table(factor(msdata,levels=names(table(scrmdata))));
+combined_ms_scrm_test=chisq.test(cbind(scrm_newtable, ms_scrmtable));
+
+legend(\"topright\",c(paste(\"Test 1 Statistics = \",combined_scrm_ms_test\$statistic,sep=\"\"), paste(\"p-value = \",format(combined_scrm_ms_test\$p.value,digits=4),sep=\"\"),paste(\"Test 2 Statistics = \",combined_ms_scrm_test\$statistic,sep=\"\"), paste(\"p-value = \",format(combined_ms_scrm_test\$p.value,digits=4),sep=\"\")))
+legend(\"topleft\",c(\"ms\",\"scrm\"), col=c(\"red\",\"blue\"), pch=16)
+
+dev.off();
+
+#cat(paste(${nsam},${t},\"|\",format(ee,digits=4),format(sdv,digits=4),\"|\",
+cat(paste(currentcase, figuretitle , \"\n\",\"|\",
+format(mean(msdata),digits=4),format(sd(msdata),digits=4),\"|\",
+format(mean(scrmdata),digits=4),format(sd(scrmdata),digits=4),\"||\",
+format(combined_scrm_ms_test\$statistic,digits=4),format(combined_scrm_ms_test\$p.value,scientific = TRUE),\"||\",
+format(combined_ms_scrm_test\$statistic,digits=4),format(combined_ms_scrm_test\$p.value,scientific = TRUE),
+sep=\"\t\"),file=\"${comparePop}\",append=TRUE);cat(\"\n\",file=\"${comparePop}\",append=TRUE);
+" > chisq.r
 
 echo "rm(list=ls());
 #source(\"../fun_src.r\");
@@ -31,30 +85,30 @@ legend(\"topleft\",c(\"ms\",\"scrm\"), col=c(\"red\",\"blue\"), pch=16)
 dev.off();
 #cat(paste(currentcase,\"|\",format(ee,digits=4),format(sdv,digits=4),\"|\",
 cat(paste(currentcase, figuretitle , \"\n\",\"|\",
-format(mean(msdata),scientific = TRUE),format(sd(msdata),scientific = TRUE),\"|\",
+format(mean(msdata),scientific = TRUE),format(sd(msdata),scientific = TRUE),\"||\",
 format(mean(scrmdata),scientific = TRUE),format(sd(scrmdata),scientific = TRUE),\"|\",format(test\$statistic,scientific = TRUE),format(test\$p.value,scientific = TRUE), 
-sep=\"\t\"),file=\"${comparePop}\",append=TRUE);cat(\"\n\",file=\"${comparePop}\",append=TRUE);" > dummy.r
+sep=\"\t\"),file=\"${comparePop}\",append=TRUE);cat(\"\n\",file=\"${comparePop}\",append=TRUE);" > ks.r
 
 foo(){
-	cut -f 2 ms_stats > msdata
-	cut -f 2 scrm_stats > scrmdata
-	echo "Pairewise_difference" > figuretitle
-	R CMD BATCH dummy.r
-	
 	cut -f 6 ms_stats > msdata
 	cut -f 6 scrm_stats > scrmdata
 	echo "Tajima_D" > figuretitle
-	R CMD BATCH dummy.r
+	R CMD BATCH ks.r
+	
+	cut -f 2 ms_stats > msdata
+	cut -f 2 scrm_stats > scrmdata
+	echo "Pairewise_difference" > figuretitle
+	R CMD BATCH chisq.r
 	
 	cut -f 8 ms_stats > msdata
 	cut -f 8 scrm_stats > scrmdata
 	echo "theta_H" > figuretitle
-	R CMD BATCH dummy.r
+	R CMD BATCH chisq.r
 	
 	cut -f 10 ms_stats > msdata
 	cut -f 10 scrm_stats > scrmdata
 	echo "H" > figuretitle
-	R CMD BATCH dummy.r
+	R CMD BATCH chisq.r
 	}
 
 #case 1 
@@ -70,7 +124,7 @@ foo
 
 echo "5_samples" > current_case
 
-ms 5 ${rep} -t ${theta} -eN 0.8 10.1 -eN 2.0 10.0 | sample_stats > ms_stats
-scrm 5 ${rep} -t ${theta} -eN 0.8 10.1 -eN 2.0 10.0 | sample_stats > scrm_stats
+ms 5 ${rep} -t ${theta} -eN 2.0 2.0 | sample_stats > ms_stats
+scrm 5 ${rep} -t ${theta} -eN 2.0 2.0 | sample_stats > scrm_stats
 
 foo
