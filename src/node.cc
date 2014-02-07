0c7da10c7f158633d40b1269e24090e77ab23064
joezhu <sha.joe.zhu@gmail.com>
2014-02-07 01:11:56 +0000

    fixing a bug for unit test on branch jz

--- src/node.cc
+++ src/node.cc
@@ -39,8 +39,8 @@ void Node::init(double height, size_t label) {
   this->set_height(height);
   this->set_label(label);
 
-  this->set_last_update(0);
-  this->set_population(0);
+//  this->set_last_update(0);
+//  this->set_population(0);
   
   if (label == 0) this->set_samples_below(1);
   else this->set_samples_below(0);
