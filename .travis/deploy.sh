8feeff0aa4c22a766e7bf0f1e49a07669437116f
Paul Staab <staab@bio.lmu.de>
2014-03-02 15:02:01 +0100

    Add first version of travis deploy script

---
+++ .travis/deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Determine if the current commit is tagged
+tag=`git describe --exact-match HEAD 2> /dev/null` || exit 0
+
+# If it is, build the .tar.gz
+echo "Building release $tag..."
+make distcheck
+release=`ls scrm-*.tar.gz` || exit 1
+
+# Copy the generated .tar.gz to the gh-pages branch
+git checkout --track origin/gh-pahes
+mv $release releases/
+git add releases/$release
+git commit -m "Add release $tag"
