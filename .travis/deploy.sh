14847b8b88fc4224750a2675b94746b80e67a07e
Paul Staab <staab@bio.lmu.de>
2014-03-02 17:33:14 +0100

    add latex support to travis script

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -4,35 +4,41 @@
 # tag=`git describe --exact-match HEAD 2> /dev/null` || exit 0
 
 # Assert that we only do this once
-CXX=${CXX:="g++"}
-[ "$CXX" == "g++" ] || exit 0
+[ "${CXX:='g++'}" == "g++" ] || exit 0
+
+# Install Latex to build the documentation
+sudo apt-get install --no-install-recommends \
+  texlive-base texlive-latex-base texlive-generic-recommended \
+  texlive-fonts-recommended texlive-fonts-extra texlive-extra-utils \
+  texlive-latex-recommended texlive-latex-extra texinfo lmodern
 
 # If it is, build the .tar.gz
 echo "Building release $tag..."
+CXXFLAGS="-O3" ./bootstrap
 make distcheck || exit 1
 release=`ls scrm-*.tar.gz` || exit 1
 
-# Copy the generated .tar.gz to the gh-pages branch
+# Configure git and make the repo use https-auth
 git config --global user.email "${GH_EMAIL}"
 git config --global user.name "${GH_NAME}"
 git remote rm origin
 git remote add origin "https://${GH_TOKEN}@github.com/paulstaab/scrm"
 git checkout --track origin/gh-pages
 
+# Copy tar.gz into releases folder and generate hashsums
 mv $release releases/
 cd releases
 md5sum $release > $release.md5
 sha512sum $release > $release.sha512
 git add $release*
 
-# Create symlinks for current release
+# Also create a symlink point to the current release
 rm scrm-current*
 ln -s $release scrm-current.tar.gz
 md5sum scrm-current.tar.gz > scrm-current.tar.gz.md5
 sha512sum scrm-current.tar.gz > scrm-current.tar.gz.sha512
 git add scrm-current.*
 
-git commit -m "Add release $tag"
-
 # And push everything to GitHub
+git commit -m "Add release $tag"
 git push origin gh-pages
