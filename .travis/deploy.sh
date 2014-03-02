4d00f248e71dd2c32c7fcce4d17a196eea56aea2
Paul Staab <staab@bio.lmu.de>
2014-03-02 16:54:42 +0100

    test travis deployment script

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 # Determine if the current commit is tagged
-tag=`git describe --exact-match HEAD 2> /dev/null` || exit 0
+# tag=`git describe --exact-match HEAD 2> /dev/null` || exit 0
+
+# Assert that we only do this once
+CXX=${CXX:="g++"}
+[ "$CXX" == "g++" ] || exit 0
 
 # If it is, build the .tar.gz
 echo "Building release $tag..."
@@ -11,5 +15,19 @@ release=`ls scrm-*.tar.gz` || exit 1
 # Copy the generated .tar.gz to the gh-pages branch
 git checkout --track origin/gh-pages
 mv $release releases/
-git add releases/$release
+cd releases
+md5sum $release > $release.md5
+sha512sum $release > $release.sha512
+git add $release*
+
+# Create symlinks for current release
+rm scrm-current*
+ln -s $release scrm-current.tar.gz
+md5sum scrm-current.tar.gz > scrm-current.tar.gz.md5
+sha512sum scrm-current.tar.gz > scrm-current.tar.gz.sha512
+git add scrm-current.*
+
 git commit -m "Add release $tag"
+
+# And push everything to GitHub
+git push origin gh-pages
