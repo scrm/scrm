9517915918b422c6899dc4c53586cee39443c784
joezhu <sha.joe.zhu@gmail.com>
2014-04-18 16:00:52 +0100

    add cluster simulation scripts

---
+++ tests/pruning_test/scrm_sim.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#$ -cwd
+#$ -V
+#$ -P bsg.prjb -q short.qb
+#$ -e ErrFiles
+#$ -o OutFiles
+#$ -N ms 
+#$ -t 1-10000
+#$ -j y
+
+source parameters
+
+#######################
+program=scrm
+cmd=${cmd}
+#######################
+
+job=${program}_
+
+prefix=${job}${rep}
+mkdir ${top_dir}"/"${prefix}
+
+fileprefix=${top_dir}"/"${prefix}"/"${prefix}
+
+{ time -p ${program} ${cmd} > ${fileprefix} -seed ${rep} ;} 2> ${fileprefix}time.text
+
+tree_file_name=${fileprefix}"Trees"
+tree_freq_name=${fileprefix}"TreeFreq"
+tmrca_name=${fileprefix}"Tmrca"
+stat_file_name=${fileprefix}"stat"
+
+grep ';' ${fileprefix} | sed -e "s/\\[.*\\]//g" > ${tree_file_name}
+grep ";" ${fileprefix} | sed -e "s/\\[//g" | sed -e "s/\\].*;//g" > ${tree_freq_name}
+hybrid-Lambda -gt ${tree_file_name} -tmrca ${tmrca_name}
+
+cat ${fileprefix} | sample_stats > ${stat_file_name}
+rm ${fileprefix} ${tree_file_name}
+
