d66ec5155bc7fa6bb36048ac86389892b7c48f38
joezhu <sha.joe.zhu@gmail.com>
2014-05-13 12:04:32 +0100

    adding process data script

--- tests/LD/cluster/process_data.sh
+++ tests/LD/cluster/process_data.sh
@@ -5,37 +5,31 @@
 #$ -e ErrFiles
 #$ -o OutFiles
 #$ -N process_data.sh
-#$ -t 1-3
+#$ -t 1-17
 #$ -j y
 
+replicate=10
 case="Constantpopsize"
 
-joblist=("ms"  "scrm" "macs")
+joblist=("ms" \
+"scrmwindow100000" "scrmwindow70000" "scrmwindow50000" "scrmwindow30000" \
+"scrmwindow10000" "scrmwindow7000" "scrmwindow5000" "scrmwindow3000" \
+"scrmwindow1000" "scrmwindow500" "scrmwindow0" \
+"macs" "macsretain1000" "macsretain10000" "macsretain100000" \
+"fastsimcoal")
 
-rep=0 # starts from zero
-echo ${joblist[${rep}]}
+#rep=$(expr $SGE_TASK_ID - 1)
+rep=0
+#for rep in $(seq 0 1 16)
+    #do
+    Job=${case}${joblist[${rep}]}
+    JobParamFile=${Job}param
+echo -e "case: ${case}\n\
+nsam: 6\n\
+replicate: ${replicate}\n\
+seqlen: 10000001\n\
+rho: 4000\n\
+job: ${Job}_" > ${JobParamFile}
+    python process_data.py ${JobParamFile}
+    #done
 
-
-
-#case: Constantpopsize
-#nsam: 6
-#replicate: 1000
-#seqlen: 10000001
-#rho: 4000
-#job: Constantpopsizems_
-#job: Constantpopsizescrmwindow100000_
-#job: Constantpopsizescrmwindow70000_
-#job: Constantpopsizescrmwindow50000_
-#job: Constantpopsizescrmwindow30000_
-#job: Constantpopsizescrmwindow10000_
-#job: Constantpopsizescrmwindow7000_
-#job: Constantpopsizescrmwindow5000_
-#job: Constantpopsizescrmwindow3000_
-#job: Constantpopsizescrmwindow1000_
-#job: Constantpopsizescrmwindow500_
-#job: Constantpopsizescrmwindow0_
-#job: Constantpopsizefastsimcoal_
-#job: Constantpopsizemacs_
-#job: Constantpopsizemacsretain1000_
-#job: Constantpopsizemacsretain10000_
-#job: Constantpopsizemacsretain100000_
