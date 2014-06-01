46d0ec117c06677787048ecc536ff701baf165f8
shajoezhu <sha.joe.zhu@gmail.com>
2014-06-02 00:29:09 +0100

    finished checking, relaunch scrm LD tests for timing

--- tests/LD/cluster/process_data.sh
+++ tests/LD/cluster/process_data.sh
@@ -11,14 +11,17 @@
 replicate=1000
 case="Constantpopsize"
 
-joblist=("ms" \
-"scrmwindow100000" "scrmwindow70000" "scrmwindow50000" "scrmwindow30000" \
+#joblist=("ms" \
+#"scrmwindow100000" "scrmwindow70000" "scrmwindow50000" "scrmwindow30000" \
+#"scrmwindow10000" "scrmwindow7000" "scrmwindow5000" "scrmwindow3000" \
+#"scrmwindow1000" "scrmwindow500" "scrmwindow0" \
+#"macsretain100000" "macsretain70000" "macsretain50000" "macsretain30000" \
+#"macsretain10000" "macsretain7000" "macsretain5000" "macsretain3000" \
+#"macsretain1000" "macsretain500" "macsretain0" \
+#"fastsimcoal")
+joblist=("scrmwindow100000" "scrmwindow70000" "scrmwindow50000" "scrmwindow30000" \
 "scrmwindow10000" "scrmwindow7000" "scrmwindow5000" "scrmwindow3000" \
-"scrmwindow1000" "scrmwindow500" "scrmwindow0" \
-"macsretain100000" "macsretain70000" "macsretain50000" "macsretain30000" \
-"macsretain10000" "macsretain7000" "macsretain5000" "macsretain3000" \
-"macsretain1000" "macsretain500" "macsretain0" \
-"fastsimcoal")
+"scrmwindow1000" "scrmwindow500" "scrmwindow0" )
 
 rep=$(expr $SGE_TASK_ID - 1)
 #rep=0
