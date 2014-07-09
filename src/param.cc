f5f969834db2e876956fa1c937309f927682f464
shajoezhu <sha.joe.zhu@gmail.com>
2014-07-09 16:54:11 +0100

    catching exception by reference

--- src/param.cc
+++ src/param.cc
@@ -307,7 +307,7 @@ void Param::parse(Model &model) {
       try {
         // Maybe read in up to 3 seeds (ms compatibility)
         for (size_t i = 1; i < 3; i++) seeds.at(i) = readNextInput<size_t>();
-      } catch (std::invalid_argument e) {
+      } catch (std::invalid_argument &e) {
           std::cout<<"ok, caught the seed is less than 3 arguments"<<endl; // DEBUG
         --this->argc_i;
       }
