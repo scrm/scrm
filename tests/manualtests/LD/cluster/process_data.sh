ccb06076ade41b29128e40ec11a1c8b1df45d2ae
shajoezhu <sha.joe.zhu@gmail.com>
2014-09-09 16:04:32 +0100

    update process_data.sh for scrm

--- tests/manualtests/LD/cluster/process_data.sh
+++ tests/manualtests/LD/cluster/process_data.sh
@@ -5,28 +5,30 @@
 #$ -e ErrFiles
 #$ -o OutFiles
 #$ -N process_data.sh
-#$ -t 1-24
+#$ -t 1-32
 #$ -j y
 
 replicate=1000
 case="Constantpopsize"
 
 #joblist=("ms" \
+"scrmwindow1000000" "scrmwindow700000" "scrmwindow500000" "scrmwindow300000" \
 #"scrmwindow100000" "scrmwindow70000" "scrmwindow50000" "scrmwindow30000" \
 #"scrmwindow10000" "scrmwindow7000" "scrmwindow5000" "scrmwindow3000" \
 #"scrmwindow1000" "scrmwindow500" "scrmwindow0" \
+#"macsretain1000000" "macsretain700000" "macsretain500000" "macsretain300000" \
 #"macsretain100000" "macsretain70000" "macsretain50000" "macsretain30000" \
 #"macsretain10000" "macsretain7000" "macsretain5000" "macsretain3000" \
 #"macsretain1000" "macsretain500" "macsretain0" \
 #"fastsimcoal")
 
-#joblist=("scrmwindow100000" "scrmwindow70000" "scrmwindow50000" "scrmwindow30000" \
-#"scrmwindow10000" "scrmwindow7000" "scrmwindow5000" "scrmwindow3000" \
-#"scrmwindow1000" "scrmwindow500" "scrmwindow0" )
-
 joblist=("scrmwindow1000000" "scrmwindow700000" "scrmwindow500000" "scrmwindow300000" \
-"macsretain1000000" "macsretain700000" "macsretain500000" "macsretain300000" \
-)
+"scrmwindow100000" "scrmwindow70000" "scrmwindow50000" "scrmwindow30000" \
+"scrmwindow10000" "scrmwindow7000" "scrmwindow5000" "scrmwindow3000" \
+"scrmwindow1000" "scrmwindow500" "scrmwindow0" )
+
+#joblist=("scrmwindow1000000" "scrmwindow700000" "scrmwindow500000" "scrmwindow300000" \
+#"macsretain1000000" "macsretain700000" "macsretain500000" "macsretain300000" )
 
 rep=$(expr $SGE_TASK_ID - 1)
 #rep=0
