f3d89be1c341eac73ebd6d2bedd204d645fea396
Paul R Staab <staab@bio.lmu.de>
2014-11-21 11:41:57 +0100

    fix build config

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -7,14 +7,18 @@ tag='TestRelease'
 # Assert that we only do this once
 [ "${CXX:='g++'}" == "g++" ] || exit 0
 
+# Build the manual
+sudo apt-get install pandoc
+./doc/create-manual.sh
+
 # Build the release
-echo "\nBuilding release $tag..."
+echo "Building release $tag..."
 CXXFLAGS="-O3" ./bootstrap || exit 1
-make distcheck || exit 1
+make dist || exit 1
 release=`ls scrm-*.tar.gz` || exit 1
 
 # Get & set up the homepage repo
-echo "\nSetting up git..."
+echo "Setting up git..."
 git clone https://github.com/scrm/scrm.github.io.git
 cd scrm.github.io
 git config user.email "${GH_EMAIL}"
@@ -23,14 +27,15 @@ echo "https://${GH_TOKEN}:@github.com" > .git/credentials
 git config credential.helper "store --file=.git/credentials"
 
 # Copy tar.gz into releases folder and generate hashes
-mv $release releases/
+echo "Adding files..." 
+mv ../$release releases/ || exit 1
 rm releases/scrm-current*
 ln -s releases/$release releases/scrm-current.tar.gz
 make
 git add releases/$release releases/scrm-current.tar.gz releases/releases*
 
 # And push everything to GitHub
-echo "\nPushing to GibHub..."
+echo "Pushing to GitHub..."
 git commit -m "Add release $tag"
 git push -fq origin master 2>&1 > /dev/null || exit 1
 rm .git/credentials
