018053cc6e7b9a2f37507db0b0a6ee5e05640e2e
Paul R Staab <staab@bio.lmu.de>
2014-11-21 13:10:01 +0100

    build windows binaries and add symlinks to current

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 # Determine if the current commit is tagged
-#tag=`git describe --exact-match HEAD 2> /dev/null` || exit 0
-tag='TestRelease'
+tag=`git describe --exact-match HEAD 2> /dev/null` || exit 0
+[ "${tag:0:1}" == "v" ] || exit 0 
+version=${tag:1}
 
 # Assert that we only do this once
 [ "${CXX:='g++'}" == "g++" ] || exit 0
 
 # Build the manual
-sudo apt-get install pandoc
+sudo apt-get install pandoc mingw-w64 zip
 ./doc/create-manual.sh
 
 # Build the release
@@ -16,6 +17,17 @@ echo "Building release $tag..."
 CXXFLAGS="-O3" ./bootstrap || exit 1
 make dist || exit 1
 release=`ls scrm-*.tar.gz` || exit 1
+
+# Build Windows Binaries
+# Build the 64bit version
+CXX=i686-w64-mingw32-g++ CXXFLAGS='-O3' LDFLAGS='-static-libgcc -static -lpthread' \
+  ./configure --host=i686-w64-mingw32 
+make && zip scrm-$version-win64.zip scrm.exe
+
+# Build the 32bit version
+CXX=i686-w64-mingw32-g++ CXXFLAGS='-O3 -m32' LDFLAGS='-static-libgcc -static -lpthread' \
+  ./configure --host=i686-w64-mingw32 
+make clean && make && zip scrm-$version-win32.zip scrm.exe
 
 # Get & set up the homepage repo
 echo "Setting up git..."
@@ -29,14 +41,19 @@ git config credential.helper "store --file=.git/credentials"
 # Copy tar.gz into releases folder and generate hashes
 echo "Adding files..." 
 mv ../$release releases/ || exit 1
+mv ../scrm-$version-* releases/ || exit 1
 rm releases/scrm-current*
-ln -s releases/$release releases/scrm-current.tar.gz
+cd releases
+ln -s $release scrm-current.tar.gz
+ln -s scrm-$version-win64.zip scrm-current-win64.zip
+ln -s scrm-$version-win32.zip scrm-current-win32.zip
+cd ..
 make
-git add releases/$release releases/scrm-current.tar.gz releases/releases*
+git add releases/$release releases/scrm-$version-* releases/scrm-current-* releases/releases*
 
 # And push everything to GitHub
 echo "Pushing to GitHub..."
-git commit -m "Add release $tag"
+git commit -m "Add release $version"
 git push -fq origin master 2>&1 > /dev/null || exit 1
 rm .git/credentials
 echo "Done"
