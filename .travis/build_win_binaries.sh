05870cca8610370c83a19f59732c5cf97f6a0406
Paul Staab <develop@paulstaab.de>
2015-06-05 09:25:08 +0200

    remove version names

--- .travis/build_win_binaries.sh
+++ .travis/build_win_binaries.sh
@@ -3,10 +3,10 @@
 # 64 bit
 CXX=i686-w64-mingw32-g++ CXXFLAGS='-O3' LDFLAGS='-static-libgcc -static -lpthread' \
   ./configure --host=i686-w64-mingw32 || exit 1
-make clean && make && zip scrm-$version-win64.zip scrm.exe doc/manual.html
+make clean && make && zip scrm-win64.zip scrm.exe doc/manual.html
 
 # 32bit 
 CXX=i686-w64-mingw32-g++ CXXFLAGS='-O3 -m32' \
   LDFLAGS='-static-libgcc -static -lpthread' \
   ./configure --host=i686-w64-mingw32 || exit 1
-make clean && make && zip scrm-$version-win32.zip scrm.exe doc/manual.html
+make clean && make && zip scrm-win32.zip scrm.exe doc/manual.html
