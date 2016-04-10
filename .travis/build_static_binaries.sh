1bd69236abd1b2769b0f679941595a1a271780bd
Paul Staab <develop@paulstaab.de>
2016-04-10 16:35:14 +0200

    update deployment script

--- .travis/build_static_binaries.sh
+++ .travis/build_static_binaries.sh
@@ -3,5 +3,6 @@
 # Build statically linked binaries
 CXXFLAGS="-O3" LDFLAGS='-static' ./configure || exit 1 
 make clean && make && \
-  tar -zcvf "scrm-x64-static.tar.gz" scrm doc/manual.html
+  tar -zcvf "scrm-x64-static.tar.gz" scrm doc/manual.html &&
+  cp scrm scrm-x64-static
 
